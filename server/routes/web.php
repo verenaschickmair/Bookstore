a95467ddd6007c09f445a2c4d097733be6985177
ver_sch <verena.schickmair@hotmail.com>
2022-03-04 10:50:45 +0100

    chore: routing basics

--- server/routes/web.php
+++ server/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/welcome', function () {
+    $name = "Fritz";
+//    return view('welcome', [
+//        "name" => "Fritz"
+//    ]);
+    return view('welcome', compact('name'));
 });
